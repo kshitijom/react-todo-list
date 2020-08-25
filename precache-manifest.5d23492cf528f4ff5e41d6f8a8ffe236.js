ccd353fae225f0a207fe75d0af4915fe2aec7341
kshitijom <mishrakshitij.646@gmail.com>
2020-08-25 20:45:35 +0530

    Updates

---
+++ precache-manifest.5d23492cf528f4ff5e41d6f8a8ffe236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa4df3f5ff7934b8b048f463f3dfedb",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "1f6fdf9433b93e09b003",
+    "url": "/react-todo-list/static/css/main.b9c476c3.chunk.css"
+  },
+  {
+    "revision": "a276061a34fffe144bd2",
+    "url": "/react-todo-list/static/js/2.2c262c6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.2c262c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f6fdf9433b93e09b003",
+    "url": "/react-todo-list/static/js/main.93ec34b5.chunk.js"
+  },
+  {
+    "revision": "80682d5f29ce18e87584",
+    "url": "/react-todo-list/static/js/runtime-main.28be1446.js"
+  }
+]);
